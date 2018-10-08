b089728ae687be89847dc2ad8da1ebfa3f18ab59
Oleksii Malyi <oleksii.malyi@synapse.com>
2018-10-08 17:58:27 +0300

    version within get flag and clean flag (overrunning) + LEDs as flags

--- my_hardware_uart/Src/main.c
+++ my_hardware_uart/Src/main.c
@@ -151,6 +151,7 @@ for (;;)
 			__HAL_UART_CLEAR_FLAG(&huart4, USART_ICR_ORECF);
 			GPIOE->ODR = GPIOE->ODR ^ 0x00000100;
 		}
+			GPIOE->ODR = GPIOE->ODR ^ 0x00001000;
 		//my_message = (uint8_t) 'T';
 		HAL_UART_Transmit(&huart4, my_data, _size, 1000);
 		
